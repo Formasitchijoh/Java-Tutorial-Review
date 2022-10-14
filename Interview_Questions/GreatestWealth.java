7b0420a761b817b06af231b51d312dbd0816be9a
Formasitchijoh <76694292+Formasitchijoh@users.noreply.github.com>
2022-10-14 15:58:56 -0700

    Computes the greatest wealth 0of a customer in a abnk

---
+++ Interview_Questions/GreatestWealth.java
@@ -0,0 +1,27 @@
+package LongestPrefixSamples;
+
+public class GreatestWealth {
+    public static  int maximumWealth(int[][] accounts) {
+
+        int GreatestWealth = 0;
+
+        for(int[] customer : accounts){
+            int wealth = 0;
+            for(int bank : customer){
+
+                wealth += bank;
+            }
+
+            GreatestWealth = Math.max(GreatestWealth,wealth);
+        }
+
+        return GreatestWealth;
+    }
+
+    public static void main(String[] args){
+
+        int[][] array = {{1,2,3,4}, {4,5,2,0}, {4 ,5, 3, 2}};
+
+       System.out.println( maximumWealth(array));
+}
+}
